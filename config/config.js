425db77a88385896423f8dfda8a265f20e0c7e1e
hadassaBrad <hadassa26162@gmail.com>
2024-05-29 12:38:08 +0300

    statrting server side!!

---
+++ config/config.js
@@ -0,0 +1,13 @@
+require('dotenv').config();;
+const { PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_PASSWORD } = process.env;
+
+module.exports = {
+    PORT, NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_PASSWORD
+};
