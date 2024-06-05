bdfe8620f9ddb9fcdb4957f555515f791f666403
hadassaBrad <hadassa26162@gmail.com>
2024-06-05 20:31:00 +0300

    addd the cors library

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const config = require('./config/config');
 const path = require('path');
 const app = express();
 app.use (express.json());
 app.use(express.urlencoded({ extended: true }));
+const cors = require('cors'); 
+
+const loginRouter=require("./routes/login")
+const aignUpRouter=require("./routes/signUp")
+const adminRouter=require("./routes/admin")
+const basketRouter=require("./routes/basketRoter")
+const galleryRouter=require("./routes/gallery")
+const sitesRouter=require("./routes/sitesRouter")
 const port = 3000;
 const SECRET_KEY = process.env.SECRET_KEY;
 router.use(session({
@@ -10,28 +19,25 @@ router.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: { secure: true } 
-  }));//להבין מה בעצם  קורה כאן ולמה זה מממוקם כאן
+  }));
+  app.use(cors());
 
+app.use("/login",(req, res, next)=>{
+  next();
+}, loginRouter);
 
-const loginRouter=require("./routes/login")
-app.use("/login",loginRouter);
-
-const aignUpRouter=require("./routes/signUp")
 app.use("/signUp",aignUpRouter);
 
-const adminRouter=require("./routes/admin")
 app.use("/admin",adminRouter);
 
-const galleryRouter=require("./routes/gallery")
 app.use("/gallery", galleryRouter);
 
-const basketRouter=require("./routes/basketRoter")
+
 app.use("/basket",basketRouter)
 
-const sitesRouter=require("./routes/sitesRouter")
 app.use("/site",sitesRouter)
 
-const basketRouter=require("./routes/")
+
 app.use("/basket",basketRouter)
 
 app.listen(port, () => {
