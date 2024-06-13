ae431034f3e0ef832b2bb1672363daed8be128ec
ruchamabricker <ruchamabricker@gmail.com>
2024-06-13 23:27:10 +0300

    login+signup working!!

--- view/src/components/BasketCard.jsx
+++ view/src/components/BasketCard.jsx
@@ -1,39 +1,38 @@
 import React from "react";
 // import Site from "./Site";
-import {useState,useEfect,useContext}from "react";
-  import    {UserContext } from '../App.jsx';
+import { useState, useEfect, useContext } from "react";
+import { UserContext } from '../App.jsx';
 function BasketCard({ site }) {
     const { user, setUser } = useContext(UserContext);
 
-    async function removeFromBasket(){
-        if(user)
-            {
-                //fetch, make render
-            }
-            else{
-              
-const siteIdToRemove = site.id;
-const currentSites = JSON.parse(localStorage.getItem("basket")) || [];
-const updatedSites = currentSites.filter(site => site.id !== siteIdToRemove);
-localStorage.setItem("basket", JSON.stringify(updatedSites));
+    async function removeFromBasket() {
+        if (user) {
+            //fetch, make render
+        }
+        else {
 
-            }
+            const siteIdToRemove = site.id;
+            const currentSites = JSON.parse(localStorage.getItem("basket")) || [];
+            const updatedSites = currentSites.filter(site => site.id !== siteIdToRemove);
+            localStorage.setItem("basket", JSON.stringify(updatedSites));
+
+        }
     }
     return (
         <>
             <h1>{site.site_name}</h1>
             <img
 
-alt={site.site_name}
-title={site.site_name} // Setting the title attribute to display the name on hover
-src={site.url}
-height={100} width={180}
-/>
-          <button  onClick={removeFromBasket}>remove From Basket</button> 
-            
+                alt={site.site_name}
+                title={site.site_name} // Setting the title attribute to display the name on hover
+                src={site.url}
+                height={100} width={180}
+            />
+            <button onClick={removeFromBasket}>remove From Basket</button>
+
         </>
     );
-        
+
 }
 
 export default BasketCard;
