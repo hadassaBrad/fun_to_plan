36a3d138100809d2f74bef26315c5256021e5223
ruchamabricker <ruchamabricker@gmail.com>
2024-06-16 23:58:34 +0300

    admin with saving new sites

--- view/src/pages/Admin.jsx
+++ view/src/pages/Admin.jsx
@@ -1,8 +1,18 @@
-import React from "react";
+import { React, useContext, useState } from 'react'
+import AdminSite from "../components/AdminSite";
+
+import { UserContext } from '../App';
+
 
 function Admin() {
+    const { user, setUser } = useContext(UserContext);
+    const [showAdminSite, setShowAdminSite] = useState(false);
+
+
     return (
-        <></>
+        <>   {user && user.role === "admin" &&
+            <button className="auth-button" onClick={() => setShowAdminSite(true)}>Admin</button>}
+            {showAdminSite && <AdminSite onClose={() => setShowAdminSite(false)} />}</>
     );
 }
 export default Admin;
