3326a0c0843add5dbc5bf875f65b87ce4690682a
hadassaBrad <hadassa26162@gmail.com>
2024-07-09 16:42:13 +0300

    with algorithem working!!!!!!

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -4,7 +4,7 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 const cors = require('cors');
 const model = require('../models/tripModel');
-const { geneticAlgorithm } = require("../services/tripService");
+const { findOptimalRoute } = require("../services/tripService");
 
 router.use(cors({
     origin: 'http://localhost:5173', // Replace with your frontend app URL
@@ -15,19 +15,19 @@ async function buildTripRoute(id, wantsGuide, startPoint, cost, numOfHours, date
     try {
         // 1. הפקת נקודת ציון מכתובת
 
-      
-                    const coordinates = await getcoordinates(startPoint);
+
+        const coordinates = await getcoordinates(startPoint);
         console.log("in controller, coordinates: " + coordinates[0] + " /" + coordinates[1]);
-      // 2. קבלת הסל שעבורו בודקים את המסלול
+        // 2. קבלת הסל שעבורו בודקים את המסלול
         const basket = await model.getBasketForTrip(id);
         console.log("the basket: ");
         console.log(basket);
-console.log("after basket");
-console.log(coordinates);
+        console.log("after basket");
+        console.log(coordinates);
         // // 3. זימון האלגוריתם
         const startingPoint = { latitude: coordinates[0], longitude: coordinates[1], cost: 0 };
         console.log("startingPoint: " + startingPoint);
-        let bestRoute = await geneticAlgorithm(basket, startingPoint, numOfHours, cost);
+        let bestRoute = await findOptimalRoute(basket, startingPoint, numOfHours, cost);
         console.log("the bestRoute: ");
         console.log(bestRoute);
         bestRoute = [startingPoint, ...bestRoute];
@@ -40,7 +40,7 @@ console.log(coordinates);
             console.log("wantsGuide");
             console.log(newRoute);
             console.log(newRoute.insertId);
-           await  addGuide(newRoute.insertId, date);
+            await addGuide(newRoute.insertId, date);
         }
         return await model.getTripRouteForUser(newRoute.insertId);
     } catch (err) {
@@ -52,7 +52,7 @@ async function addGuide(tripId, date) {
     try {
         console.log("trip id");
         console.log(tripId);
-        const allGuidesByDate = await model.getGuidesByDate( date);
+        const allGuidesByDate = await model.getGuidesByDate(date);
         console.log("all guides by date in trip controler");
         console.log(allGuidesByDate);
         console.log(allGuidesByDate.id);
