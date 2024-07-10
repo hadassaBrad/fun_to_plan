42bb513eb0c971f531e609c2f02a1cb14a5ecb9c
hadassaBrad <hadassa26162@gmail.com>
2024-07-10 10:40:09 +0300

    final   project

--- services/mailService.js
+++ services/mailService.js
@@ -1,35 +1,4 @@
-// const nodemailer = require('nodemailer');
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: 'funtoplan100@gmail.com',
-//     pass: 'RHfuntoplan100@'
-//      //כתובת המייל הזאת עשיין לא קימת כי לא הצלחתי לתפתוח בלי SMS
-//      //אחרי שפותחים אותה צריך להגידר אימות דו שלבי, יתר הפרטים בGPT
-//   }
-// });
-
-// const sendMail = (to, subject, text) => {
-//   const mailOptions = {
-//     from: 'funtoplan100@gmail.com',
-//     to,
-//     subject,
-//     text
-//   };
-
-//   return new Promise((resolve, reject) => {
-//     transporter.sendMail(mailOptions, (error, info) => {
-//       if (error) {
-//         reject(error);
-//       } else {
-//         resolve(info);
-//       }
-//     });
-//   });
-// };
-
-// module.exports = sendMail;
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
@@ -38,7 +7,7 @@ const transporter = nodemailer.createTransport({
     user: 'funtoplan1@gmail.com',
     pass: 'znip ngnb tgom ptoq',
 
-    // השתמש בסיסמת האפליקציה שיצרת
+  
   }
 });
 
